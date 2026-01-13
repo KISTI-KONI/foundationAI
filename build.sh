6dd8a6ce9c60e4f8f4eb89caf1c2e8f3817f273a
Evergyu <Evergyu@github.com>
2026-01-13 17:32:07 +0900

    Add: Build with Local sif

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# 실행 위치 기준으로 container 디렉토리로 이동
+cd container
+
+# 1) Docker 이미지로부터 SIF 빌드
+singularity build --fakeroot torch290.sif docker://pytorch/pytorch:2.9.0-cuda12.6-cudnn9-devel
+
+# 2) 로컬 def 파일로 sandbox 빌드
+singularity build --fakeroot --sandbox ./exaone/ torch290_git_local.def
+
+
+
